2dddbb84fffcfc67f073b172833eefe4075bd625
sujata1406 <60836765+sujata1406@users.noreply.github.com>
2026-07-12 17:00:40 +0530

    Create Assignment_23_1_Pool.py

---
+++ Assignment_23/Assignment_23_1_Pool.py
@@ -0,0 +1,37 @@
+import time
+import multiprocessing
+import os
+
+
+def SumEven(No):
+    print("Process is running with PID: ",os.getpid())
+    Sum=0
+
+    for i in range(1,No+1):
+        if i % 2==0:
+            Sum=Sum+i
+    return Sum
+
+
+def main():
+
+    Data=[10000000,20000000,30000000,40000000]
+    Result=[]
+
+
+    pobj=multiprocessing.Pool()
+
+    Result=pobj.map(SumEven,Data)
+
+    pobj.close()
+    pobj.join()
+
+
+    print("Result is :")
+    print(Result)
+
+
+
+
+if __name__=="__main__":
+    main()
