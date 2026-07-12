4140a21f5066df3ec56c8ed1a75e0612d62f2ef7
sujata1406 <60836765+sujata1406@users.noreply.github.com>
2026-07-12 16:54:23 +0530

    Create Assignment_19_1_PowerOf.py

---
+++ Assignment_19/Assignment_19_1_PowerOf.py
@@ -0,0 +1,13 @@
+# Power of two of number
+
+Powerof=lambda No:No**2
+
+def main():
+    Value=int(input("enter number : "))
+    
+    Ret=Powerof(Value)
+    print("Power of two is : ",Ret)
+        
+
+if __name__=="__main__":
+    main()
